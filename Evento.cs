0f0bdc00095b34d5463b83bd37bc67fdac6d1cb0
andreaacomelli <comelliandrea21@gmail.com>
2024-07-20 15:26:04 +0200

    aggiunta classe main e metodi classe evento

--- Evento.cs
+++ Evento.cs
@@ -2,19 +2,46 @@ class Evento
 {
 
   private string titolo;
-  private string data;
+  private DateTime data;
   private int postiTot, postiPrenotati = 0;
 
-  public string Data { get => data; set => data = value; }
+  public DateTime Data { get => data; set => data = value; }
   public string Titolo { get => titolo; set => titolo = value; }
-  public int PostiTot { get => postiTot; set => postiTot = value; }
-  public int PostiPrenotati { get => postiPrenotati; set => postiPrenotati = value; }
+  public int PostiTot { get => postiTot; }
+  public int PostiPrenotati { get => postiPrenotati; }
 
-  public Evento(string titolo, string data, int postiPrenotati, int postiTot)
+  public Evento(string titolo, DateTime data, int postiPrenotati, int postiTot)
   {
     this.titolo = titolo;
     this.data = data;
     this.postiPrenotati = postiPrenotati;
     this.postiTot = postiTot;
+  }
+
+  public void Prenota()
+  {
+    int dataComparata = data.CompareTo(DateTime.Now); //compareTo restituisce valore intero che se > 0 significa che data è futuro ad oggi
+
+    if (postiTot > postiPrenotati && dataComparata > 0)
+    {
+      postiPrenotati += 1;
+    }
+    else { Console.WriteLine("L'evento è gia passato o i posti prenotabili sono esauriti"); }
+  }
+
+  public void Disdici()
+  {
+    int dataComparata = data.CompareTo(DateTime.Now); //compareTo restituisce valore intero che se > 0 significa che data è futuro ad oggi
+
+    if (postiPrenotati == 0 && dataComparata > 0)
+    {
+      postiPrenotati -= 1;
+    }
+    else { Console.WriteLine("L'evento è gia passato o i posti prenotabili sono esauriti"); }
+  }
+
+  public override string ToString()
+  {
+    return data.ToString("MMMM dd, yyyy") + "." + titolo + postiPrenotati + postiTot;
   }
 }
---
+++ Main.cs
@@ -0,0 +1,37 @@
+﻿namespace prova;
+
+class Principale
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Inserisci il titolo dell'evento: ");
+    string titolo = Console.ReadLine();
+
+    Console.WriteLine("Inserisci la data dell'evento (formato yyyy-MM-dd): ");
+    string dataString = Console.ReadLine();
+    DateTime data = DateTime.Parse(dataString);
+
+    Console.WriteLine("Inserisci il numero di posti prenotati: ");
+    int postiPrenotati = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Inserisci il numero totale di posti: ");
+    int postiTot = int.Parse(Console.ReadLine());
+
+    Evento evento = new(titolo, data, postiPrenotati, postiTot);
+
+    Console.WriteLine("Cosa desideri fare: prenotare o disdire un posto?");
+    string scelta = Console.ReadLine().ToLower();
+
+    if (scelta == "prenotare")
+    {
+      evento.Prenota();
+      evento.ToString();
+    }
+    else if (scelta == "disdire")
+    {
+      evento.Disdici();
+      evento.ToString();
+    }
+    else { Console.WriteLine("Inserisci un'opzione valida"); }
+  }
+}
