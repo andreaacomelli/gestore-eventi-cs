191f74493c9e054e1f47b9a2b0dfd754d5904af5
andreaacomelli <comelliandrea21@gmail.com>
2024-07-20 16:40:12 +0200

    aggiunta try catch per input nulli

--- Main.cs
+++ Main.cs
@@ -4,34 +4,50 @@ class Principale
 {
   static void Main(string[] args)
   {
-    Console.WriteLine("Inserisci il titolo dell'evento: ");
-    string titolo = Console.ReadLine();
-
-    Console.WriteLine("Inserisci la data dell'evento (formato yyyy-MM-dd): ");
-    string dataString = Console.ReadLine();
-    DateTime data = DateTime.Parse(dataString);
-
-    Console.WriteLine("Inserisci il numero di posti prenotati: ");
-    int postiPrenotati = int.Parse(Console.ReadLine());
-
-    Console.WriteLine("Inserisci il numero totale di posti: ");
-    int postiTot = int.Parse(Console.ReadLine());
-
-    Evento evento = new(titolo, data, postiPrenotati, postiTot);
-
-    Console.WriteLine("Cosa desideri fare: prenotare o disdire un posto?");
-    string scelta = Console.ReadLine().ToLower();
-
-    if (scelta == "prenotare")
+    try
     {
-      evento.Prenota();
-      Console.WriteLine(evento.ToString());
+      Console.WriteLine("Inserisci il titolo dell'evento: ");
+      string titolo = Console.ReadLine();
+
+      Console.WriteLine("Inserisci la data dell'evento (formato yyyy-MM-dd): ");
+      string dataString = Console.ReadLine();
+      DateTime data = DateTime.Parse(dataString);
+
+      Console.WriteLine("Inserisci il numero di posti prenotati: ");
+      int postiPrenotati = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Inserisci il numero totale di posti: ");
+      int postiTot = int.Parse(Console.ReadLine());
+      Evento evento = new(titolo, data, postiPrenotati, postiTot);
+
+      Console.WriteLine("Cosa desideri fare: prenotare o disdire un posto?");
+      string scelta = Console.ReadLine().ToLower();
+
+      if (scelta == "prenotare")
+      {
+        evento.Prenota();
+        Console.WriteLine(evento.ToString());
+      }
+      else if (scelta == "disdire")
+      {
+        evento.Disdici();
+        Console.WriteLine(evento.ToString());
+      }
+      else { Console.WriteLine("Inserisci un'opzione valida"); }
+
     }
-    else if (scelta == "disdire")
+    catch (ArgumentNullException e)
     {
-      evento.Disdici();
-      Console.WriteLine(evento.ToString());
+      Console.WriteLine(e.Message);
     }
-    else { Console.WriteLine("Inserisci un'opzione valida"); }
+    catch (FormatException e)
+    {
+      Console.WriteLine("Formato non valido: " + e.Message);
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine("Si è verificato un errore: " + e.Message);
+    }
+
   }
 }
--- obj/Debug/net8.0/prova.AssemblyInfo.cs
+++ obj/Debug/net8.0/prova.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prova")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0bdc00095b34d5463b83bd37bc67fdac6d1cb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75e9bf3935bf0b031c5afc706153ae757cf44b18")]
 [assembly: System.Reflection.AssemblyProductAttribute("prova")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prova")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
